b45d4bbc52f7a7a80294f04fa417870f19afa1f5
tlfrank2 <tyreselfranklin@gmail.com>
2024-12-03 10:48:32 -0600

    Create loadfooter.js

---
+++ MIS7375/Homework4/loadfooter.js
@@ -0,0 +1,14 @@
+// loadFooter.js
+function loadFooter() {
+    fetch('footer.html')
+        .then(response => {
+            if (!response.ok) throw new Error('Network response was not ok');
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('footer-placeholder').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading footer:', error));
+}
+
+window.onload = loadFooter;
